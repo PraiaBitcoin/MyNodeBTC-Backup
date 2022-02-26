0552d790d532d37a29ef9fd936a529ca346c5980
Fernando Motolese <81449156+motolese@users.noreply.github.com>
2022-02-26 18:44:23 -0300

    Update install.sh
    Install service and time to automate the mynodebackup. 
    Before running, change the permission:
    chmod 755 install.sh
    sudo ./install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+chmod 755 backupmynode.sh
+cp config/backupmynode.service /etc/systemd/system/backupmynode.service
+cp config/backupmynode.timer /etc/systemd/system/backupmynode.timer
+systemctl enable backupmynode.timer
+systemctl start backupmynode.timer
