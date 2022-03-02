cdc792d689bb3b13b44125def66a3abeb7929384
Fernando Motolese <81449156+motolese@users.noreply.github.com>
2022-03-02 14:55:50 -0300

    Update install.sh
    Added mv .config to complete the GPG configuration!

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 chmod 755 backupmynode.sh
+mv .config /mnt/hdd/BACKUP/.config
 cp config/backupmynode.service /etc/systemd/system/backupmynode.service
 cp config/backupmynode.timer /etc/systemd/system/backupmynode.timer
 systemctl enable backupmynode.timer
