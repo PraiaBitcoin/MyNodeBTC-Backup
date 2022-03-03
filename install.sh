e89a5c24363b28999db8540a12483a9ebad25227
Praia Bitcoin #BitcoinBeachBR <94765727+PraiaBitcoin@users.noreply.github.com>
2022-03-03 19:47:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@ chmod 755 backupmynode.sh
 if [ ! -f /mnt/hdd/mynode/backup/.config ]
 then
     echo copiando Config
-    cp .config /mnt/hdd/mynode/backup/.config
+    cp /home/admin/MyNodeBTC-Backup/.config /mnt/hdd/mynode/backup/.config
 fi
-cp config/backupmynode.service /etc/systemd/system/backupmynode.service
-cp config/backupmynode.timer /etc/systemd/system/backupmynode.timer
+
+cp /home/admin/MyNodeBTC-Backup/config/backupmynode.service /etc/systemd/system/backupmynode.service
+cp /home/admin/MyNodeBTC-Backup/config/backupmynode.timer /etc/systemd/system/backupmynode.timer
+systemctl enable backupmynode.service
+systemctl start backupmynode.service
 systemctl enable backupmynode.timer
 systemctl start backupmynode.timer
