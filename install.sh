977976ffe0c7d2aafe23cc99a7298ec962215895
Praia Bitcoin <94765727+PraiaBitcoin@users.noreply.github.com>
2022-08-18 13:29:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 chmod 755 /home/admin/MyNodeBTC-Backup/backupmynode.sh
 if [ ! -f /mnt/hdd/mynode/backup/.config ]
 then
+    echo Criando diretório de backup
+    mkdir /mnt/hdd/mynode/backup
     echo copiando Config
     cp /home/admin/MyNodeBTC-Backup/.config /mnt/hdd/mynode/backup/.config
 fi
