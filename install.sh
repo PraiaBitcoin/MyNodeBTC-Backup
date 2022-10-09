d0a71344a7fda3264740644fa382292ec3e33e22
Antonio S Schug <antoniosidineischug@gmail.com>
2022-10-09 12:59:52 -0300

    Update install.sh
    Added gdrive installation

--- install.sh
+++ install.sh
@@ -8,6 +8,14 @@ then
     cp /home/admin/MyNodeBTC-Backup/.config /mnt/hdd/mynode/backup/.config
 fi
 
+if [ ! -f /usr/bin/gdrive ]
+then
+  echo Instalando Google Drive Helper
+  curl -sSL https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz |tar xfzv - -C /usr/bin/
+  chmod 555 /usr/bin/gdrive
+  gdrive list
+fi
+
 cp /home/admin/MyNodeBTC-Backup/config/backupmynode.service /etc/systemd/system/backupmynode.service
 cp /home/admin/MyNodeBTC-Backup/config/backupmynode.timer /etc/systemd/system/backupmynode.timer
 systemctl enable backupmynode.service
